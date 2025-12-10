93e977990eeb46f4a2c8a3bb92905d5305742fb9
Alfredo <aalfonzo@google.com>
2025-12-10 18:41:19 +0000

    moved hardcoded strings to .env

--- cautils/test_schema_relationships.py
+++ cautils/test_schema_relationships.py
@@ -1,11 +1,17 @@
 from . import data_agent
 
+import os
+from dotenv import load_dotenv
+
+# Load variables from .env file
+load_dotenv()
+
 
 def test_gen_schema_relationships():
     print(
         data_agent._gen_schema_relationships(
-            project_id="as-alf-argolis",
-            location="global",
+            project_id=os.environ["GOOGLE_CLOUD_PROJECT"],
+            location=os.environ["GOOGLE_CLOUD_DER"],
             data_source_references_path="sample_definitions/sample1/datasourceReferences.yaml",
         )
     )
